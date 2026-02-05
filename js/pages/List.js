d2b390dcd84752d39598317bfde841a75cdf23c8
SethariusXO <sethjbomers@gmail.com>
2026-02-05 17:19:43 +1100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -71,7 +71,7 @@ export default {
                                 <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
                             </td>
                             <td class="cc">
-                                <p>{{ record.cc }} CC</p>
+                                <p>{{ record.cc }}CC</p>
                             </td>
                         </tr>
                     </table>
