ea6f5e248c4e58a2b8003835a9d58e4b307ea113
SethariusXO <sethjbomers@gmail.com>
2026-02-06 18:35:58 +1100

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -46,7 +46,7 @@ export default {
                         <h1>#{{ selected + 1 }} {{ entry.user }}Hz</h1>
                         <h2>{{ entry.completed.length + entry.verified.length}} Beaten In Total</h2>
                         <h3>{{ entry.total }} Points</h3>
-                        <h3 v-if="entry.verified.length > 0">Verified ({{ entry.verified.length}})</h3>
+                        <h2 v-if="entry.verified.length > 0">Verified ({{ entry.verified.length}})</h2>
                         <table class="table">
                             <tr v-for="score in entry.verified">
                                 <td class="rank">
