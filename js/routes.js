e7bf0ca7f33d040d758fd83f2e36266e55c80874
SethariusXO <sethjbomers@gmail.com>
2026-03-07 18:27:43 +1100

    Add route for demonlist component

--- js/routes.js
+++ js/routes.js
@@ -3,7 +3,7 @@ import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 
 export default [
-    { path: '/', component: List },
+    { path: '/demonlist', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
 ];
