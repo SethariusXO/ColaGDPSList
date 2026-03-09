2ac7bc7042d79755d2a12dd24544d5668acd22ba
SethariusXO <sethjbomers@gmail.com>
2026-03-09 17:55:32 +1100

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
+import PList from './pages/PList';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 
 export default [
     { path: '/', component: List },
+    { path: '/Plist', component: PList },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
 ];
