d551c7e43394d733ea94a8fd6d620b8cba2598dc
SethariusXO <sethjbomers@gmail.com>
2026-04-10 13:31:08 +1000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -57,7 +57,7 @@ export default {
                     </ul>
                     <h2>Records ({{ level.victors }})</h2>
                     <p>
-                    Points When Completed: {{ level(selected + 1, 100, level.percentToQualify) }} 
+                    Points When Completed: {{ level.Points}} 
                     </p>
                     <p v-if="selected + 1 <= 75">List Percentage: <strong>{{ level.percentToQualify }}%</strong></p>
                     <p v-else-if="selected +1 <= 150"><strong>100%</strong> or better to qualify</p>
