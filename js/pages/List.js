8c9e862dca3d5930eddc8de72ce88ebefa76c76a
SethariusXO <sethjbomers@gmail.com>
2026-04-10 16:12:19 +1000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -98,7 +98,7 @@ export default {
                             </li>
                         </ol>
                     </template>
-                    <h3>Check out our youtube channel <a href="https://www.youtube.com/channel/UCsWrgEXq34GyLqK8KJetKbg" target="_blank">HERE</a></h3>
+                    <h3 class="type-label-md">Check out our youtube channel <a href="https://www.youtube.com/channel/UCsWrgEXq34GyLqK8KJetKbg" target="_blank">HERE</a></h3>
                     <h3>Extra Info</h3>
                     <p>
                     "CC" is the abbreviation for Coins Collected
