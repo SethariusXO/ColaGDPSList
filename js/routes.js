5176a425025fdfeea570f25315492f2778468611
SethariusXO <sethjbomers@gmail.com>
2026-05-29 19:19:19 +1000

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -5,4 +5,6 @@ import Roulette from './pages/Roulette.js';
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
+    { path: '/roulette', component: Roulette },
+    { path: '/packs', component: Maps },
 ];
